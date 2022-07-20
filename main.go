a6c1235be1590a1befe5e2f9373a295c989f5900
shenlidego <109620800+shenlidego@users.noreply.github.com>
2022-07-21 00:13:25 +0800

    xiaowu

--- main.go
+++ main.go
@@ -78,7 +78,6 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/genshin"        // 原神抽卡
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/gif"            // 制图
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/github"         // 搜索GitHub仓库
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/guessmusic"     // 猜歌
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hs"             // 炉石
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/hyaku"          // 百人一首
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/image_finder"   // 关键字搜图
@@ -169,9 +168,9 @@ func init() {
 	// 直接写死 AccessToken 时，请更改下面第二个参数
 	token := flag.String("t", "", "Set AccessToken of WSClient.")
 	// 直接写死 URL 时，请更改下面第二个参数
-	url := flag.String("u", "ws://127.0.0.1:6703", "Set Url of WSClient.")
+	url := flag.String("u", "ws://127.0.0.1:6701", "Set Url of WSClient.")
 	// 默认昵称
-	adana := flag.String("n", "椛椛", "Set default nickname.")
+	adana := flag.String("n", "小伍", "Set default nickname.")
 	prefix := flag.String("p", "/", "Set command prefix.")
 	runcfg := flag.String("c", "", "Run from config file.")
 	save := flag.String("s", "", "Save default config to file and exit.")
@@ -258,3 +257,4 @@ func main() {
 			ctx.SendChain(message.Text(kanban.Kanban()))
 		})
 	zero.RunAndBlock(config.Z, process.GlobalInitMutex.Unlock)
+}
